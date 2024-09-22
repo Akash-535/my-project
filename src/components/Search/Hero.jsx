f6add8633825a0f25a501d2dd44b0f903ad4cb01
akash-535 <akashbish9e@gmail.com>
2024-09-22 20:19:14 +0530

    image change

--- src/components/Search/Hero.jsx
+++ src/components/Search/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
- import loremImage from '../../assets/images/png/layerImage.png';
+ import loremImage from '../../assets/images/png/man.png';
  import location from '../../assets/images/svg/location.svg';
  import calender from '../../assets/images/svg/calender.svg';
  import people from '../../assets/images/svg/people.svg';
@@ -11,8 +11,8 @@ const Hero = () => {
       <div className="container flex justify-center items-center">
         <div className="card_div shadow p-5 border border-black rounded-lg">
      <div className="image">
-       <div className='rounded-lg w-[344px] h-[252px] overflow-hidden'>
-       <img className=' hover:scale-[1.2] duration-300 w-full' src={loremImage} alt="loremImage" />  
+       <div className=' w-[367px] h-[374px] overflow-hidden rounded-md'>
+       <img className=' hover:scale-[1.1] duration-300 w-full rounded-md' src={loremImage} alt="loremImage" />  
        </div>    
      </div>
      <p className='pt-5 text-2xl font-semibold'>Lorem ipsum dolor sit amet.</p>
